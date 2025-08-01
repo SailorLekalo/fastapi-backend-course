fdaca50bfc5a8142477ca3fc94616d4b1f0a12f6
SailorLekalo <shamshev-andrei@yandex.ru>
2025-08-01 18:40:49 +0300

    Позадача 4. Методы записи и чтения из БД инкапсулированы в класс Storage

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -4,7 +4,6 @@ from enum import Enum
 import json
 import random
 
-app = FastAPI()
 class Status(Enum):
     OPEN = "OPEN"
     IN_PROCESS = "IN_PROCESS"
@@ -15,22 +14,33 @@ class Task(BaseModel):
     status: Status
     descr:str
 
-def get_tasks_from_database():
-    with open("database.json", "r") as db:
-        try:
-            tasks = json.load(db)
-        except json.JSONDecodeError:
-            tasks = {}
-    return tasks
+class Storage():
+    __storage: str|None
+    def __init___(self, storage:str|None):
+        self.storage = storage
+        if not os.path.exists(self.storage):
+             with open(self.filename, "w") as f:
+                json.dump({}, f)
+        
+    def get_tasks_from_database(self):
+        with open(storage, "r") as db:
+            try:
+                tasks = json.load(db)
+            except json.JSONDecodeError:
+                tasks = {}
+        return tasks
 
-def dump_tasks_to_database(tasks):
-    with open("database.json","w") as db:
-        json.dump(tasks,db,indent=4)
+    def dump_tasks_to_database(self,tasks):
+        with open(storage,"w") as db:
+            json.dump(tasks,db,indent=4)
+
+app = FastAPI()
+storage = Storage("database.json")
 
 @app.get("/tasks")
 def get_tasks():
-    with open("database.json","r") as db:
-        return get_tasks_from_database()
+    return storage.get_tasks_from_database()
+    
 
 @app.post("/tasks")
 def create_task(task:Task):
@@ -38,33 +48,33 @@ def create_task(task:Task):
     task = task.model_dump(mode = "json")
     task_id = abs(hash(random.randbytes(32)))
     
-    tasks = get_tasks_from_database()
+    tasks = storage.get_tasks_from_database()
             
     tasks[task_id] = task
 
-    dump_tasks_to_database(tasks)
+    storage.dump_tasks_to_database(tasks)
 
     return f"Task created with id {task_id}"
 
 @app.put("/tasks/{task_id}")
 def update_task(task_id: int, new_status: Status):
 
-    tasks = get_tasks_from_database()
+    tasks = storage.get_tasks_from_database()
     try:
         tasks[str(task_id)]['status'] = new_status.value
     except KeyError:
         return "No task with this ID"
     
-    dump_tasks_to_database(tasks)
+    storage.dump_tasks_to_database(tasks)
             
     return "Task status changed succesfully"
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    tasks = get_tasks_from_database()
+    tasks = storage.get_tasks_from_database()
     try:
         tasks.pop(str(task_id))
     except KeyError:
         return "No task with this ID"
-    dump_tasks_to_database(tasks)
+    storage.dump_tasks_to_database(tasks)
     return "Task deleted succesfully"
